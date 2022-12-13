04473b98183393df4d1a1b921e6b0f2d3b8d93c2
unknown <shannoncoleman16@gmail.com>
2022-12-12 21:33:53 -0500

    added and aligned header

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,15 @@
-var weatherUrl  = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key};"
-var weatherAPI  = "https: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={74f819854d296034845f9c7d9b053ea8};"
+var weatherUrl  = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+
+document.querySelector("#search-button").addEventListener("click", function() {
+var weatherApi  = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={74f819854d296034845f9c7d9b053ea8}";
+
+    fetch(weatherApi)
+    .then(function(response){
+        if  (response.ok) {
+            response.json()
+            .then(function(data){ 
+                console.log(data);
+            });
+        }
+    });
+});
