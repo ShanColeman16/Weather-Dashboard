1301837a74c12d731a7f9d7d39bd463852269404
unknown <shannoncoleman16@gmail.com>
2022-12-12 19:51:03 -0500

    added weather api link

---
+++ assets/script.js
@@ -0,0 +1,2 @@
+var weatherUrl  = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key};"
+var weatherAPI  = "https: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={74f819854d296034845f9c7d9b053ea8};"
