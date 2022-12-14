437bbac0c98f1475d0d45b06b694097c88a5cc8d
unknown <shannoncoleman16@gmail.com>
2022-12-13 22:07:24 -0500

    added state search button

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,29 @@
-var weatherUrl  = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
-document.querySelector("#search-button").addEventListener("click", function() {
-var weatherApi  = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={74f819854d296034845f9c7d9b053ea8}";
+document.querySelector("#search-button").addEventListener("click", function () {
+    var weatherApi = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=74f819854d296034845f9c7d9b053ea8";
+    var latApi = "http://api.openweathermap.org/geo/1.0/direct?q=Charlotte,NC,US &limit=1&appid=74f819854d296034845f9c7d9b053ea8"
 
-    fetch(weatherApi)
-    .then(function(response){
-        if  (response.ok) {
-            response.json()
-            .then(function(data){ 
-                console.log(data);
-            });
+
+    fetch(latApi)
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        console.log(data);
+                    }
+                    )
+            }
         }
-    });
+        )
+
+    // fetch(weatherApi)
+    //     .then(function (response) {
+    //         if (response.ok) {
+    //             response.json()
+    //                 .then(function (data) {
+    //                     console.log(data);
+    //                 });
+    //         }
+    //     });
 });
