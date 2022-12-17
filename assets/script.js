a8321b2b3cd17948ebaa17288d8204392953b927
unknown <shannoncoleman16@gmail.com>
2022-12-16 22:47:21 -0500

    added lat and long to api and forcast to query selector

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 // var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 var searchButton = document.querySelector("#search-button");
 var searchInput = document.querySelector("#search-city-Input");
-var q = "hello";
+var forecast  = document.querySelector("#forecast");
 searchButton.addEventListener("click", function()   { 
-var weatherUrl  ='https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=74f819854d296034845f9c7d9b053ea8';
-
+var weatherUrl  ='https://api.openweathermap.org/geo/2.5/weather?q=lat=${lat}&lon=${long}&limit=5&appid=" + 74f819854d296034845f9c7d9b053ea8 + "&units=imperial"';
+console.log ("hello")
 
 fetch(weatherUrl)
   .then(function (response) {
