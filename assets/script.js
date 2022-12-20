30b3305453ec32a9e969e655212566efc9a315f8
unknown <shannoncoleman16@gmail.com>
2022-12-19 23:53:57 -0600

    added current wather dec and class

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,22 @@
 // var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 var searchButton = document.querySelector("#search-button");
 var searchInput = document.querySelector("#search-city-Input");
-var forecast  = document.querySelector("#forecast");
-searchButton.addEventListener("click", function()   { 
-var weatherUrl  ='https://api.openweathermap.org/geo/2.5/weather?q=lat=${lat}&lon=${long}&limit=5&appid=" + 74f819854d296034845f9c7d9b053ea8 + "&units=imperial"';
-console.log ("hello")
+var futureForecast  = document.querySelector("#forecast");
+var currentWeather =searchCity+today;
+///searchButton.addEventListener("click", function()   { 
+var weatherUrl  ='https://api.openweathermap.org/geo/2.5/weather?q=lat=${lat}&lon=${long}&limit=5&appid=" + 74f819854d296034845f9c7d9b053ea8';
 
 fetch(weatherUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    console.log();
   })
   .catch(function (error) {
     console.log(error);
   });
 
-});
+;
 
 
